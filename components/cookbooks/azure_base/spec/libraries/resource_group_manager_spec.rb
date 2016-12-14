4fb763f1e80f9c7852db596bc81042864721e961
Heriberto Rodriguez <heriberto.rodriguez13@gmail.com>
2016-12-14 13:05:10 -0600

    Added Resource Group MNGR test

--- components/cookbooks/azure_base/spec/libraries/resource_group_manager_spec.rb
+++ components/cookbooks/azure_base/spec/libraries/resource_group_manager_spec.rb
@@ -5,13 +5,14 @@ require File.expand_path('../../../libraries/resource_group_manager.rb', __FILE_
 
 describe AzureBase::ResourceGroupManager do
   before do
-    @azure_credentials = YAML.load_file('spec/azure_creds.yml')
+    @azure_creds = YAML.load_file('spec/azure_creds.yml')
 
     node_test = {
+      asdf: sdf
 
     }
 
-    rg_mgr = AzureBase::ResourceGroupManager.new()
+    rg_mgr = AzureBase::ResourceGroupManager.new(node_test)
   end
 
   context 'creating rg_mgr' do
