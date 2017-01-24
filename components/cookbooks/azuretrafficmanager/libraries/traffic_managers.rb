d0804446ff0e8eff71809dadead76c39174592d6
bilal.naeem <bilal.naeem@confiz.com>
2017-01-24 17:02:57 +0500

    Refactor Traffic Manager Add recipe. Moved functions to TrafficManagers Library. Updated unit test.

--- components/cookbooks/azuretrafficmanager/libraries/traffic_managers.rb
+++ components/cookbooks/azuretrafficmanager/libraries/traffic_managers.rb
@@ -1,10 +1,13 @@
 require 'fog/azurerm'
 require 'chef'
 require ::File.expand_path('../../../azure_base/libraries/logger', __FILE__)
+require ::File.expand_path('../../../azure_base/libraries/utils', __FILE__)
 
 # Cookbook Name:: azuretrafficmanager
 class TrafficManagers
   attr_accessor :traffic_manager_service
+
+  attr_accessor :entries
 
   def initialize(resource_group, profile_name, dns_attributes)
     raise ArgumentError, 'resource_group is nil' if resource_group.nil?
@@ -65,6 +68,14 @@ class TrafficManagers
     traffic_manager_profile
   end
 
+  def initialize_traffic_manager(dns_attributes, resource_group_names, ns_path_parts, gdns_attributes, listeners, subdomain)
+    endpoints = initialize_endpoints(get_public_ip_fqdns(dns_attributes, resource_group_names, ns_path_parts))
+    dns_config = initialize_dns_config(dns_attributes, gdns_attributes, subdomain)
+    monitor_config = initialize_monitor_config(listeners)
+    traffic_routing_method = gdns_attributes[:traffic_routing_method]
+    TrafficManager.new(traffic_routing_method, dns_config, monitor_config, endpoints)
+  end
+
   private
 
   def serialize_endpoints(endpoints)
@@ -87,5 +98,81 @@ class TrafficManagers
       end
     end
     serialized_array
+  end
+
+  def initialize_endpoints(targets)
+    endpoints = []
+    targets.each do |target|
+      index = targets.index(target)
+      location = target.split('.').reverse[3]
+      endpoint_name = 'endpoint_' + location + '_' + index.to_s
+      endpoint = EndPoint.new(endpoint_name, target, location)
+      endpoint.set_endpoint_status(EndPoint::Status::ENABLED)
+      endpoint.set_weight(1)
+      endpoint.set_priority(index + 1)
+      endpoints.push(endpoint)
+    end
+    endpoints
+  end
+
+  def initialize_dns_config(dns_attributes, gdns_attributes, subdomain)
+    domain = dns_attributes[:zone]
+    domain_without_root = domain.split('.').reverse.join('.').partition('.').last.split('.').reverse.join('.')
+    dns_name = if !subdomain.empty?
+                 subdomain + '.' + domain_without_root
+               else
+                 domain_without_root
+               end
+    relative_dns_name = dns_name.tr('.', '-').slice!(0, 60)
+    Chef::Log.info('The Traffic Manager FQDN is ' + relative_dns_name)
+    display_traffic_manager_fqdn(relative_dns_name)
+
+    dns_ttl = gdns_attributes[:ttl]
+    DnsConfig.new(relative_dns_name, dns_ttl)
+  end
+
+  def initialize_monitor_config(listeners)
+    protocol = listeners.tr('[]"', '').split(' ')[0].upcase
+
+    monitor_port = listeners.tr('[]"', '').split(' ')[1]
+    monitor_path = '/'
+    MonitorConfig.new(protocol, monitor_port, monitor_path)
+  end
+
+  def get_public_ip_fqdns(dns_attributes, resource_group_names, ns_path_parts)
+    platform_name = ns_path_parts[5]
+    plat_name = platform_name.gsub(/-/, '').downcase
+    load_balancer_name = "lb-#{plat_name}"
+    public_ip_fqdns = []
+    credentials = Utils.get_credentials(dns_attributes[:tenant_id], dns_attributes[:client_id], dns_attributes[:client_secret])
+    lb = AzureNetwork::LoadBalancer.new(dns_attributes[:tenant_id], dns_attributes[:client_id], dns_attributes[:client_secret], dns_attributes[:subscription])
+    pip = AzureNetwork::PublicIp.new(credentials, dns_attributes[:subscription])
+
+    resource_group_names.each do |resource_group_name|
+      load_balancer = lb.get(resource_group_name, load_balancer_name)
+      next if load_balancer.nil?
+
+      public_ip_id = load_balancer.frontend_ip_configurations[0].public_ipaddress_id
+      public_ip_name = public_ip_id.split('/')[8]
+      public_ip = pip.get(resource_group_name, public_ip_name)
+      public_ip_fqdn = public_ip.fqdn
+      Chef::Log.info('Obtained public ip fqdn ' + public_ip_fqdn + ' to be used as endpoint for traffic manager')
+      public_ip_fqdns.push(public_ip_fqdn)
+    end
+    public_ip_fqdns
+  end
+
+  def display_traffic_manager_fqdn(dns_name)
+    fqdn = dns_name + '.' + 'trafficmanager.net'
+    ip = ''
+    entries = []
+    entries.push(name: fqdn, values: ip)
+    entries_hash = {}
+    entries.each do |entry|
+      key = entry[:name]
+      entries_hash[key] = entry[:values]
+    end
+    @entries = entries
+    puts "***RESULT:entries=#{JSON.dump(entries_hash)}"
   end
 end
--- components/cookbooks/azuretrafficmanager/recipes/add.rb
+++ components/cookbooks/azuretrafficmanager/recipes/add.rb
@@ -7,95 +7,6 @@ require File.expand_path('../../../azure_lb/libraries/load_balancer.rb', __FILE_
 require File.expand_path('../../../azure/libraries/public_ip.rb', __FILE__)
 require File.expand_path('../../../azure_base/libraries/utils.rb', __FILE__)
 require 'chef'
-require 'azure_mgmt_network'
-
-::Chef::Recipe.send(:include, Utils)
-::Chef::Recipe.send(:include, AzureNetwork)
-
-def get_public_ip_fqdns(dns_attributes, resource_group_names, ns_path_parts)
-  platform_name = ns_path_parts[5]
-  plat_name = platform_name.gsub(/-/, '').downcase
-  load_balancer_name = "lb-#{plat_name}"
-  public_ip_fqdns = []
-  credentials = Utils.get_credentials(dns_attributes['tenant_id'], dns_attributes['client_id'], dns_attributes['client_secret'])
-  lb = AzureNetwork::LoadBalancer.new(dns_attributes['tenant_id'], dns_attributes['client_id'], dns_attributes['client_secret'], dns_attributes['subscription'])
-  pip = AzureNetwork::PublicIp.new(credentials, dns_attributes['subscription'])
-
-  resource_group_names.each do |resource_group_name|
-    load_balancer = lb.get(resource_group_name, load_balancer_name)
-    next if load_balancer.nil?
-
-    public_ip_id = load_balancer.frontend_ip_configurations[0].public_ipaddress_id
-    public_ip_name = public_ip_id.split('/')[8]
-    public_ip = pip.get(resource_group_name, public_ip_name)
-    public_ip_fqdn = public_ip.fqdn
-    Chef::Log.info('Obtained public ip fqdn ' + public_ip_fqdn + ' to be used as endpoint for traffic manager')
-    public_ip_fqdns.push(public_ip_fqdn)
-  end
-  public_ip_fqdns
-end
-
-def initialize_monitor_config
-  listeners = node.workorder.payLoad.lb[0][:ciAttributes][:listeners]
-  protocol = listeners.tr('[]"', '').split(' ')[0].upcase
-
-  monitor_port = listeners.tr('[]"', '').split(' ')[1]
-  monitor_path = '/'
-  MonitorConfig.new(protocol, monitor_port, monitor_path)
-end
-
-def display_traffic_manager_fqdn(dns_name)
-  fqdn = dns_name + '.' + 'trafficmanager.net'
-  ip = ''
-  entries = []
-  entries.push(name: fqdn, values: ip)
-  entries_hash = {}
-  entries.each do |entry|
-    key = entry[:name]
-    entries_hash[key] = entry[:values]
-  end
-  node.set[:entries] = entries
-  puts "***RESULT:entries=#{JSON.dump(entries_hash)}"
-end
-
-def initialize_dns_config(dns_attributes, gdns_attributes)
-  domain = dns_attributes['zone']
-  domain_without_root = domain.split('.').reverse.join('.').partition('.').last.split('.').reverse.join('.')
-  subdomain = node['workorder']['payLoad']['Environment'][0]['ciAttributes']['subdomain']
-  dns_name = if !subdomain.empty?
-               subdomain + '.' + domain_without_root
-             else
-               domain_without_root
-             end
-  relative_dns_name = dns_name.tr('.', '-').slice!(0, 60)
-  Chef::Log.info('The Traffic Manager FQDN is ' + relative_dns_name)
-  display_traffic_manager_fqdn(relative_dns_name)
-
-  dns_ttl = gdns_attributes['ttl']
-  DnsConfig.new(relative_dns_name, dns_ttl)
-end
-
-def initialize_endpoints(targets)
-  endpoints = []
-  for i in 0..targets.length-1
-    location = targets[i].split('.').reverse[3]
-    endpoint_name = 'endpoint_' + location + '_' + i.to_s
-    endpoint = EndPoint.new(endpoint_name, targets[i], location)
-    endpoint.set_endpoint_status(EndPoint::Status::ENABLED)
-    endpoint.set_weight(1)
-    endpoint.set_priority(i + 1)
-    endpoints.push(endpoint)
-  end
-  endpoints
-end
-
-def initialize_traffic_manager(public_ip_fqdns, dns_attributes, gdns_attributes)
-  endpoints = initialize_endpoints(public_ip_fqdns)
-  dns_config = initialize_dns_config(dns_attributes, gdns_attributes)
-  monitor_config = initialize_monitor_config
-  traffic_routing_method = gdns_attributes['traffic-routing-method']
-  TrafficManager.new(traffic_routing_method, dns_config, monitor_config, endpoints)
-end
 
 def get_resource_group_names
   ns_path_parts = node['workorder']['rfcCi']['nsPath'].split('/')
@@ -123,9 +34,6 @@ def get_traffic_manager_resource_group(resource_group_names, profile_name, dns_a
   end
   nil
 end
-#################################################
-#                                               #
-#################################################
 
 # set the proxy if it exists as a cloud var
 Utils.set_proxy(node.workorder.payLoad.OO_CLOUD_VARS)
@@ -134,16 +42,18 @@ ns_path_parts = node['workorder']['rfcCi']['nsPath'].split('/')
 cloud_name = node['workorder']['cloud']['ciName']
 dns_attributes = node['workorder']['services']['dns'][cloud_name]['ciAttributes']
 gdns_attributes = node['workorder']['services']['gdns'][cloud_name]['ciAttributes']
+listeners = node['workorder']['payLoad']['lb'][0][:ciAttributes][:listeners]
+subdomain = node['workorder']['payLoad']['Environment'][0]['ciAttributes']['subdomain']
 
 begin
   resource_group_names = get_resource_group_names
-  public_ip_fqdns = get_public_ip_fqdns(dns_attributes, resource_group_names, ns_path_parts)
-  traffic_manager = initialize_traffic_manager(public_ip_fqdns, dns_attributes, gdns_attributes)
-
-  platform_name = ns_path_parts[5]
-  profile_name = 'trafficmanager-' + platform_name
-
+  profile_name = 'trafficmanager-' + ns_path_parts[5]
   resource_group_name = get_traffic_manager_resource_group(resource_group_names, profile_name, dns_attributes)
+
+  traffic_manager_processor = TrafficManagers.new(resource_group_name, profile_name, dns_attributes)
+  traffic_manager = traffic_manager_processor.initialize_traffic_manager(dns_attributes, resource_group_names, ns_path_parts, gdns_attributes, listeners, subdomain)
+  node.set[:entries] = traffic_manager_processor.entries
+
   if resource_group_name.nil?
     include_recipe 'azure::get_platform_rg_and_as'
     resource_group_name = node['platform-resource-group']
