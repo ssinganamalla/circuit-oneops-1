cb13c80821c66def0f2edcd2459b2d038ce5e401
Samawia Moin <samawia.moin@confiz.com>
2017-01-30 15:26:59 +0500

    Made peer review changes

--- components/cookbooks/azure/libraries/virtual_machine_manager.rb
+++ components/cookbooks/azure/libraries/virtual_machine_manager.rb
@@ -1,6 +1,5 @@
 module AzureCompute
   class VirtualMachineManager
-
     attr_accessor :compute_service,
                   :initial_user,
                   :private_ip,
@@ -8,11 +7,10 @@ module AzureCompute
                   :compute_ci_id,
                   :token
 
-
     def initialize(node)
       @cloud_name = node['workorder']['cloud']['ciName']
       @compute_service =
-          node['workorder']['services']['compute'][@cloud_name]['ciAttributes']
+        node['workorder']['services']['compute'][@cloud_name]['ciAttributes']
       @keypair_service = node['workorder']['payLoad']['SecuredBy'].first
       @server_name = node['server_name']
       @resource_group_name = node['platform-resource-group']
@@ -45,14 +43,12 @@ module AzureCompute
       end
     end
 
-    def create_or_update_vm()
+    def create_or_update_vm
       OOLog.info('Resource group name: ' + @resource_group_name)
 
       @ip_type = 'public'
       @ip_type = 'private' if @express_route_enabled
       OOLog.info('ip_type: ' + @ip_type)
-
-
 
       @storage_profile = AzureCompute::StorageProfile.new(@creds)
       @storage_profile.resource_group_name = @resource_group_name
@@ -73,14 +69,14 @@ module AzureCompute
       vm_hash[:name] = @server_name
       vm_hash[:resource_group] = @resource_group_name
       vm_hash[:availability_set_id] =
-          @compute_client.availability_sets.get(@resource_group_name, @resource_group_name).id
+        @compute_client.availability_sets.get(@resource_group_name, @resource_group_name).id
       vm_hash[:location] = @compute_service[:location]
 
       # hardware profile values
       vm_hash[:vm_size] = @size_id
 
       # storage profile values
-      vm_hash[:storage_account_name] = @storage_profile.get_storage_account_name()
+      vm_hash[:storage_account_name] = @storage_profile.get_storage_account_name
       vm_hash[:publisher] = @imageID[0]
       vm_hash[:offer] = @imageID[1]
       vm_hash[:sku] = @imageID[2]
@@ -98,16 +94,15 @@ module AzureCompute
       vm_hash[:ssh_key_data] = @keypair_service[:ciAttributes][:public]
 
       # network profile values
-      vm_hash[:network_interface_card_id] = @network_profile.build_network_profile( @compute_service[:express_route_enabled],
-                                                                                    @compute_service[:resource_group],
-                                                                                    @compute_service[:network],
-                                                                                    @compute_service[:network_address].strip,
-                                                                                    (@compute_service[:subnet_address]).split(','),
-                                                                                    (@compute_service[:dns_ip]).split(','),
-                                                                                    @ip_type,
-                                                                                    @secgroup_name )
+      vm_hash[:network_interface_card_id] = @network_profile.build_network_profile(@compute_service[:express_route_enabled],
+                                                                                   @compute_service[:resource_group],
+                                                                                   @compute_service[:network],
+                                                                                   @compute_service[:network_address].strip,
+                                                                                   (@compute_service[:subnet_address]).split(','),
+                                                                                   (@compute_service[:dns_ip]).split(','),
+                                                                                   @ip_type,
+                                                                                   @secgroup_name)
 
-      puts "VM_HASH: #{vm_hash.inspect}"
       @private_ip = @network_profile.private_ip
       # create the virtual machine
       begin
@@ -122,7 +117,7 @@ module AzureCompute
       OOLog.info('Subscription id is: ' + @compute_service[:subscription])
       start_time = Time.now.to_i
       @ip_type = 'public'
-      @ip_type = 'private' if @express_route_enabled == true
+      @ip_type = 'private' if @express_route_enabled == 'true'
       OOLog.info('ip_type: ' + @ip_type)
       begin
         vm = @compute_client.servers.get(@resource_group_name, @server_name)
@@ -131,15 +126,13 @@ module AzureCompute
         else
           # retrive the vhd name from the VM properties and use it to delete the associated VHD in the later step.
           vhd_uri = vm.os_disk_vhd_uri
-          storage_account  = (vhd_uri.split(".").first).split("//").last
+          storage_account = vhd_uri.split('.').first.split('//').last
           datadisk_uri = nil
-          if vm.data_disks.count > 0
-            datadisk_uri = vm.data_disks[0].vhd_uri
-          end
+          datadisk_uri = vm.data_disks[0].vhd_uri if vm.data_disks.count > 0
           Chef::Log.info("Deleting Azure VM: '#{@server_name}'")
-          #delete the VM from the platform resource group
+          # delete the VM from the platform resource group
 
-          Chef::Log.info("VM is deleted") if vm.destroy
+          Chef::Log.info('VM is deleted') if vm.destroy
 
           return storage_account, vhd_uri, datadisk_uri
         end
@@ -152,7 +145,6 @@ module AzureCompute
         duration = end_time - start_time
         OOLog.info("Deleting VM took #{duration} seconds")
       end
-
     end
   end
 end
--- components/cookbooks/azure/recipes/del_node.rb
+++ components/cookbooks/azure/recipes/del_node.rb
@@ -1,58 +1,34 @@
 require File.expand_path('../../../azure_base/libraries/logger.rb', __FILE__)
 require 'fog/azurerm'
 
-#set the proxy if it exists as a cloud var
+# set the proxy if it exists as a cloud var
 Utils.set_proxy(node.workorder.payLoad.OO_CLOUD_VARS)
-
-# delete the NIC from the platform specific resource group
-def delete_nic(vm_client)
-    start_time = Time.now.to_i
-    nic_name = Utils.get_component_name("nic",vm_client.compute_ci_id)
-    network_profile = AzureNetwork::NetworkInterfaceCard.new(vm_client.creds)
-    network_profile.rg_name = vm_client.resource_group_name
-    network_profile.get(nic_name).destroy
-    end_time = Time.now.to_i
-    duration = end_time - start_time
-    OOLog.info("Deleting NIC '#{nic_name}' in #{duration} seconds")
-end
-
-#Delete public ip assocaited with the VM
-def delete_publicip(vm_client)
-  begin
-    start_time = Time.now.to_i
-    if ip_type == 'public'
-      public_ip_name = Utils.get_component_name("publicip",vm_client.compute_ci_id)
-      public_ip = AzureNetwork::PublicIp.new(vm_client.creds)
-      public_ip.get(vm_client.resource_group_name, public_ip_name).destroy
-    end
-    end_time = Time.now.to_i
-    duration = end_time - start_time
-    OOLog.info("Deleting public ip '#{public_ip_name}' in #{duration} seconds")
- end
-end
-
-# invoke recipe to get credentials
-include_recipe "azure::get_credentials"
 
 # get platform resource group and availability set
 include_recipe 'azure::get_platform_rg_and_as'
 
 # delete the VM
-  vm_client = AzureCompute::VirtualMachineManager.new(node)
-  storage_account, vhd_uri, datadisk_uri = vm_client.delete_vm
-  node.set["storage_account"] = storage_account
-  node.set["vhd_uri"] = vhd_uri
-  node.set["datadisk_uri"] = datadisk_uri
+vm_client = AzureCompute::VirtualMachineManager.new(node)
+storage_account, vhd_uri, datadisk_uri = vm_client.delete_vm
+node.set['storage_account'] = storage_account
+node.set['vhd_uri'] = vhd_uri
+node.set['datadisk_uri'] = datadisk_uri
 
-  # delete the NIC. A NIC is created with each VM, so we will delete the NIC when we delete the VM
-  delete_nic(vm_client)
+# delete the NIC. A NIC is created with each VM, so we will delete the NIC when we delete the VM
+nic_name = Utils.get_component_name('nic', vm_client.compute_ci_id)
+network_profile = AzureNetwork::NetworkInterfaceCard.new(vm_client.creds)
+network_profile.delete(vm_client.resource_group_name, nic_name)
 
-  # public IP must be deleted after the NIC.
-  delete_publicip(vm_client)
+# public IP must be deleted after the NIC.
+if vm_client.ip_type == 'public'
+  public_ip_name = Utils.get_component_name('publicip', vm_client.compute_ci_id)
+  public_ip = AzureNetwork::PublicIp.new(vm_client.creds)
+  public_ip.delete(vm_client.resource_group_name, public_ip_name)
+end
 
-  # delete the blobs
-  # Delete both Page blob(vhd) and Block Blob from the storage account
-  # Delete both osdisk and datadisk blob
-  include_recipe "azure::del_blobs"
+# delete the blobs
+# Delete both Page blob(vhd) and Block Blob from the storage account
+# Delete both osdisk and datadisk blob
+include_recipe 'azure::del_blobs'
 
-OOLog.info("Exiting azure delete compute")
+OOLog.info('Exiting azure delete compute')
